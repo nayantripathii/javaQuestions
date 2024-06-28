73e2fb03db7d26bf87630edd2d31c3e21abdb6af
nayantripathii <nayan.tripathie@gmail.com>
2024-06-28 23:24:42 +0530

    this is my first file

---
+++ twoSum.java
@@ -0,0 +1,18 @@
+public class twoSum {
+
+}
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    int[] a= new int[2];
+                    a[0]=i;
+                    a[1]=j;
+                    return a;
+                }
+            }
+        }
+        return nums;
+    }
+}
